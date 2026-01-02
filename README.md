dae2201fc8c8bf3343a2795ccac346694bcc1174
Nova <aelithron@gmail.com>
2026-01-02 00:46:01 -0700

    a little more work before bed :3

---
+++ README.md
@@ -0,0 +1,5 @@
+**⚠️ This project is not completed yet, and will have bugs! DO NOT use this in production.**
+# SimpleGraves ![IMG](https://hackatime-badge.hackclub.com/U08RJ1PEM7X/SimpleGraves)
+A powerful Minecraft graves (aka death chests) plugin that doesn't overcomplicate.
+Compatible with Spigot servers and forks (like Paper). \
+Made for Hack Club [Flavortown](https://flavortown.hackclub.com)!
--- src/main/java/dev/novatea/simplegraves/SimpleGraves.java
+++ src/main/java/dev/novatea/simplegraves/SimpleGraves.java
@@ -1,12 +1,21 @@
 package dev.novatea.simplegraves;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SimpleGraves extends JavaPlugin {
-
+public final class SimpleGraves extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        if (e.getKeepInventory()) return;
+
     }
 }
